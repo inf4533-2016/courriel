4a16332afd7439cb702f88e5280e44be784fb7ae
Wojciech Fraczak <fraczak@gmail.com>
2016-04-07 22:55:08 -0400

    Example of encryption

--- src/client/index.js
+++ src/client/index.js
@@ -4,30 +4,95 @@ var NodeRSA = require('node-rsa');
 var CryptoJS = require('crypto-js');
 var superagent = require('superagent');
 
-var header = $('#header');
-var body = $('#body');
-var footer = $('#footer');
-
-header.append("c'est entête...");
-
-body.append( $('<div>').append('... eh body'));
-
-footer.append('... footer');
-
-var password = prompt("Pass phrase");
+var $header = $('#header');
+var $focus = $('#focus'); 
+var $body = $('#body');
 
 var key = null;
-if (ld.isEmpty(encryptedKey)) {
-    key = new NodeRSA({b: 512});
-    encryptedKey = CryptoJS.AES.encrypt(key.exportKey(), password).toString();
-    superagent
-        .post("/storeEncryptedKey")
-        .send({encryptedKey: encryptedKey} )
-        .end(console.log.bind(console));
-} else {
-    !function (pem) {
-        key = new NodeRSA(pem);
-    } (CryptoJS.AES.decrypt(encryptedKey, password).toString(CryptoJS.enc.Utf8));
-}
-console.log(key.exportKey());
+var etat = null;
 
+
+var addAddress = function() {
+    var name = prompt("Name");
+    var pem = prompt("Public key");
+    if (ld.isEmpty(pem)) {
+        var randomKey = new NodeRSA({b: 512});
+        pem = randomKey.exportKey('public');
+    }
+    etat.yp[pem] = {name: name};
+    superagent
+        .post("/addAddress")
+        .send({pem:pem, name:name})
+        .end(console.log.bind(console));
+};
+
+var newMessage = function($textarea) {
+    var text = $textarea.val();
+    var address = Object.keys(etat.yp)[0];
+    var pubKey = new NodeRSA(address);
+    var msg = {date:new Date(), to: address, msg: pubKey.encrypt(text,'base64')};
+    console.log(JSON.stringify(msg, null, 2));
+    superagent
+        .post("/postMessage")
+        .send(msg)
+        .end(console.log.bind(console));
+}
+
+var redraw_view = function() {
+    var $content;
+    var $newAddress;
+    var $newMessage;
+    var $textarea;
+    switch($focus.val()) {
+    case "inbox" :
+        $content =  ["List of messages..."];
+        break;
+    case "write" :
+        $textarea = $('<textarea>');
+        $newMessage = $('<button>').append("New Message").click(newMessage.bind(null, $textarea) );
+        $content = [ "Compose a new message...", $textarea, $newMessage ];
+        break;
+    case "yp" :
+        $newAddress = $('<button>').append("New Address").click(addAddress);
+        $content =  ["Address book...", $newAddress ];
+        break;
+    };
+    $body.empty().append($content);
+};
+
+$focus.change( redraw_view );
+
+superagent
+    .get("/etat")
+    .send()
+    .end( function(err, res) {
+        if (err) {
+            console.error(err);
+        } else {
+            etat = res.body;
+            var password = null;
+            if (ld.isEmpty(etat.encryptedKey)) {
+                key = new NodeRSA({b: 512});
+                password = prompt("New pass phrase");
+                etat.encryptedKey = CryptoJS.AES.encrypt(key.exportKey(), password).toString();
+                superagent
+                    .post("/storeEncryptedKey")
+                    .send({encryptedKey: etat.encryptedKey} )
+                    .end(console.log.bind(console));
+            } else {
+                while (true) {
+                    var pem = null;
+                    try {
+                        password = prompt("Your pass phrase");
+                        pem = CryptoJS.AES.decrypt(etat.encryptedKey, password).toString(CryptoJS.enc.Utf8);
+                        key = new NodeRSA(pem);
+                        break;
+                    } catch (e) {
+                        alert("Try again...");
+                    }
+                }
+            }
+            console.log(key.exportKey());
+        };
+        redraw_view();
+    });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 var express = require("express");
 var body_parse  = require('body-parser');
+var json_file_object = require("json-file-object");
 
 var app = express();
 
-var encryptedKey = null;
+var etat = json_file_object({
+    file: "etat.json",
+    value: {
+        encryptedKey: null,
+        yp: {},
+        letters: []
+    }
+});
 
 app.locals.pretty = true;
 
 app.get("/", function(req,res) {
-    res.render("courriel.jade", {
-        encryptedKey:encryptedKey 
-    });
+    res.render("courriel.jade");
 });
 
 app.post("/storeEncryptedKey", body_parse.json(), function(req,res) {
-    encryptedKey = req.body.encryptedKey;
-    console.log(encryptedKey);
-    res.json({msg:"ok"});
+    etat.encryptedKey = req.body.encryptedKey;
+    res.json("ok");
+});
+
+app.post("/addAddress", body_parse.json(), function(req,res) {
+    etat.yp[req.body.pem] = req.body;
+    res.json("Address added");
+});
+
+
+app.post("/postMessage", body_parse.json(), function(req,res) {
+    etat.letters.push(req.body);
+    res.json("Message posted");
+});
+
+
+app.get("/etat", function(req,res) {
+    res.json(etat);
 });
 
 app.use(express.static('public'));
+
 app.listen(8888);
